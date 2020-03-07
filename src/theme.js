b20791510c8ee762082636c4ff73c6001e395865
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-03-07 20:37:17 +0200

    define theme

---
+++ src/theme.js
@@ -0,0 +1,90 @@
+export default {
+  palette: {
+    primary: {
+      light: '#FFFFFF',
+      main: '#0061FF',
+      dark: '#000000'
+    },
+    primary1: {
+      main: '#503DFF',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#a7d129',
+      main: '#616f39',
+      dark: '#3e432e'
+    },
+    background: {
+      paper: '#FFFFFF',
+      main: '#06162b'
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#000',
+      body: '#8B8B97'
+    },
+    action: {
+      hover: '#efebe915'
+    },
+    border: {
+      outline: '#D4D4E1'
+    }
+  },
+  shape: {
+    roundedInputBorderRadius: 8,
+    cardBorderRaius: 8
+  },
+  spacing: {
+    unit: 8
+  },
+  typography: {
+    extraLarge: {
+      fontSize: 70,
+      fontWeight: 'bold',
+      fontFamily: 'heebo-medium'
+    },
+    large: {
+      fontSize: 40,
+      fontFamily: 'heebo-medium'
+    },
+    addNewLabel: {
+      fontSize: 35,
+      fontFamily: 'heebo-medium'
+    },
+    screenHeader: {
+      fontSize: 18,
+      fontFamily: 'heebo-medium'
+    },
+    h1: {
+      fontSize: 40,
+      fontFamily: 'heebo-medium'
+    },
+    h2: {
+      fontSize: 20,
+      fontFamily: 'heebo-medium'
+    },
+    h3: {
+      fontSize: 16,
+      fontFamily: 'heebo-medium'
+    },
+    tab: {
+      fontSize: 16
+    },
+    text1: {
+      fontSize: 18
+    },
+    caption: {
+      fontSize: 16
+    },
+    smallText: {
+      fontSize: 12
+    },
+    smallBtn: {
+      fontSize: 14
+    },
+    headerLabel: {
+      fontSize: 14,
+      fontWeight: 'bold'
+    }
+  }
+}
